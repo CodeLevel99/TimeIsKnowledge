ff9a3aed2656bcdf45ad6d9a32f28d5b9134b4c9
Michael Fan <Michael Fan>
2017-03-05 06:13:47 -0500

    File

---
+++ pagescript.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    var clicked = false;
+    var countup;
+    var second = 0;
+    var millisecond = 0;
+    var secondstring = "00s ";
+
+    function startTimer() {
+        countup = setInterval(function() {
+            millisecond++;
+
+            if(millisecond == 100)
+            {
+                millisecond = 0;
+                second++;
+                if(second < 10)
+                {
+                    secondstring = "0" + second.toString() + "s ";
+                }
+                else
+                {
+                    secondstring = second.toString() + "s ";
+                }
+            }
+
+            $("#clock-button").text(secondstring + millisecond.toString() + "ms");
+        }, 10);
+    }
+
+    function stopTimer() {
+        clearInterval(countup);
+    }
+
+    $("#fact-container").hide();
+
+    $("#clock-button").click(function() {
+        if(clicked == false)
+        {
+            clicked = true;
+            startTimer();
+            $("#clock").css("animationPlayState", "running");
+            $("#clock-button").css("animationPlayState", "running");
+            
+        }
+        else if(clicked == true)
+        {
+            stopTimer();
+            $("#clock").css("animationPlayState", "paused");
+            $("#clock-button").css("animationPlayState", "paused");
+            $("#clock").fadeOut("fast");
+            $("#fact-container").show("slow");
+        }
+    });
+
+    $("#fact-button").click(function() {
+        clicked = false;
+        second = 0;
+        millisecond = 0;
+        secondstring = "00s ";
+        $("#fact-container").hide("fast");
+        $("#clock").fadeIn("slow");
+        $("#clock-button").text("00s 00ms");
+    });
+});
